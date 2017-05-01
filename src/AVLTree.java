50e1c8b24bba263af7edec46c162ece15d3e5580
Abdelrahman <abdoumohamed96@gmail.com>
2017-05-01 17:07:49 +0200

    make fields in node public + fix null pointer exception

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,5 +1,3 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
 import java.util.Comparator;
 import java.util.Collection;
 import java.util.Iterator;
@@ -29,129 +27,104 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
         return null;
     }
 
-    private Node<E> balance(Node<E> node){
-        if(node==null)
-            return node;
+    private Node<E> balance(Node<E> node) {
+        if (node == null)
+            return null;
 
-        System.out.println(node.getLeft().getHeight());
-        System.out.println(node.getRight().getHeight());
+        int hleft = height(node.left);
+        int hright = height(node.right);
 
-        if(node.getLeft().getHeight() - node.getRight().getHeight() > 1)
-            if(node.getLeft().getLeft().getHeight() >= node.getLeft().getRight().getHeight())
-                node=rotateLeft(node);
+        if (hleft - hright > 1)
+            if (height(node.left.left) >= height(node.left.right))
+                node = rotateLeft(node);
             else
-                node=doubleRotateLeft(node);
-        else
-        if(node.getRight().getHeight() - node.getLeft().getHeight() > 1)
-            if(node.getRight().getRight().getHeight() >= node.getRight().getLeft().getHeight())
-                node=rotateRight(node);
+                node = doubleRotateLeft(node);
+        else if (hright - hleft > 1)
+            if (height(node.right.right) >= height(node.right.left))
+                node = rotateRight(node);
             else
-                node=doubleRotateRight(node);
+                node = doubleRotateRight(node);
 
-        node.setHeight( max(node.getLeft().getHeight(),
-                node.getRight().getHeight()) + 1);
+        node.height = max(height(node.left), height(node.right)) + 1;
         return node;
 
     }
 
-    private Node<E> rotateLeft(Node<E> k2){
-        Node<E> k1=k2.getLeft();
+    private int height(Node<E> t) {
+        return t == null ? -1 : t.height;
+    }
 
-        k2.setLeft(k1.getRight());
-        k1.setRight(k2);
 
-        k2.setHeight(   max(k2.getLeft().getHeight(),
-                k2.getRight().getHeight()) + 1);
-        k1.setHeight(   max(k1.getLeft().getHeight(),
-                k2.getHeight()) + 1);
+    private Node<E> rotateLeft(Node<E> k2) {
+        Node<E> k1 = k2.left;
+
+        k2.left = k1.right;
+        k1.right = k2;
+
+        k2.height = max(k2.left.height, k2.right.height) + 1;
+        k1.height= max(k1.left.height, k2.height) + 1;
         return k1;
     }
 
-    private Node<E> rotateRight(Node<E> k1){
-        Node<E> k2 = k1.getRight();
+    private Node<E> rotateRight(Node<E> k1) {
+        Node<E> k2 = k1.right;
 
-        k1.setRight(k2.getLeft());
-        k2.setLeft(k1);
+        k1.right = k2.left;
+        k2.left = k1;
 
-        k1.setHeight( max(  k1.getLeft().getHeight() ,
-                k1.getRight().getHeight() ) +1 );
+        k1.height = max(height(k1.left), height(k1.right)) + 1;
 
-        k2.setHeight( max( k2.getRight().getHeight() ,
-                k1.getHeight() ) +1);
+        k2.height = max(height(k2.right), height(k1)) + 1;
         return k2;
     }
 
-    private Node<E> doubleRotateLeft(Node<E> k3){
-        k3.setLeft(rotateRight(k3.getLeft()));
-        return  rotateLeft(k3);
+    private Node<E> doubleRotateLeft(Node<E> k3) {
+        k3.left = rotateRight(k3.left);
+        return rotateLeft(k3);
     }
 
-    private Node<E> doubleRotateRight(Node<E> k1){
-        k1.setRight(rotateLeft(k1.getRight()));
+    private Node<E> doubleRotateRight(Node<E> k1) {
+        k1.right = rotateLeft(k1.right);
         return rotateRight(k1);
     }
 
     private Node<E> insert(E data, Node<E> node) {
-        if(node==null){
+        if (node == null) {
             size++;
             return new Node<>(data, null, null);
         }
 
-        int compareResult=myCompare(data, node.getData());
+        int compareResult = myCompare(data, node.getData());
 
-        if(compareResult < 0)
-            node.setLeft(insert(data, node.getLeft()));
-        else if(compareResult > 0)
-            node.setRight(insert(data, node.getRight()));
+        if (compareResult < 0)
+            node.left = insert(data, node.left);
+        else if (compareResult > 0)
+            node.right = insert(data, node.right);
         else // Ignore duplicates
             ;
 
         return balance(node);
 
-//        node.setHeight(max(root.getLeft().getHeight(), root.getRight().getHeight()) +1);
-//
-//        int balanceFactor=getBalanceFactor(node);
-//
-//        // Left Left Case
-//        if(balanceFactor>1 && myCompare(data, node.getLeft().getData()) < 0)
-//            return rotateRight(node);
-//
-//        // Right Right Case
-//        if(balanceFactor<-1 && myCompare(data, node.getRight().getData()) > 0)
-//            return rotateLeft(node);
-//
-//        // Left Right Case
-//        if(balanceFactor > 1 && myCompare(data, node.getLeft().getData()) > 0){
-//            node.setLeft(rotateLeft(node.getLeft()));
-//            return rotateRight(node);
-//        }
-//
-//        // Right Left Case
-//        if(balanceFactor < -1 && myCompare(data, node.getRight().getData()) < 0){
-//            node.setRight(rotateRight(node.getRight()));
-//            return rotateLeft(node);
-//        }
-//
-//        return node;
-
     }
 
-    private void displayTree(Node<E> node){
-        if(node!=null){
-            System.out.println(node.getData() + " ");
-            displayTree(node.getLeft());
-            displayTree(node.getRight());
-        }
+    private void displayTree(Node<E> node) {
+        if (node == null)
+            return;
+
+        displayTree(node.left);
+        System.out.println(node.getData() + " ");
+        displayTree(node.right);
+
     }
 
     private int myCompare(E first, E second) {
-        if(comparator != null)
+        if (comparator != null)
             return comparator.compare(first, second);
 
-        if(!(first instanceof Comparable && second instanceof Comparable))
+        if (!(first instanceof Comparable && second instanceof Comparable))
             throw new ClassCastException();
 
-        return ((Comparable)first).compareTo( second );
+        return ((Comparable) first).compareTo(second);
     }
 
     private boolean delete(Object data, Node<E> root) {
@@ -159,31 +132,27 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
     }
 
     private E maximum(Node<E> node) {
-        if(node==null)
+        if (node == null)
             return node.getData();
 
-        while(node.getRight()!=null)
-            node=node.getRight();
+        while (node.right != null)
+            node = node.right;
 
         return node.getData();
     }
 
-//    private E minimum(Node<E> root) {
-//        return null;
-//    }
-
-    private E minimum(Node<E> node){
-        if(node==null)
+    private E minimum(Node<E> node) {
+        if (node == null)
             return node.getData();
 
-        while(node.getLeft()!=null)
-            node=node.getLeft();
+        while (node.left != null)
+            node = node.left;
 
         return node.getData();
     }
 
-    private boolean isEmpty(Node<E> node){
-        return (node==null);
+    private boolean isEmpty(Node<E> node) {
+        return (node == null);
     }
 
     @Override
@@ -193,7 +162,7 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
 
     @Override
     public boolean add(E e) {
-        if(root == null) {
+        if (root == null) {
             root = insert(e, root);
             return root != null;
         }
@@ -209,10 +178,10 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
     public boolean addAll(Collection<? extends E> collection) {
         boolean inserted = false;
         for (E e : collection) {
-            if(e == null)
+            if (e == null)
                 throw new NullPointerException("Null elements are not allowed");
 
-            if(add(e))
+            if (add(e))
                 inserted = true;
         }
 
@@ -230,52 +199,18 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
     }
 
     @Override
-
     public int height() {
         return 0;
     }
 
-
-    private int height(Node<E> node){
-        if(node==null)
-            return 0;
-        return node.getHeight();
+    private int max(int x, int y) {
+        return Integer.max(x, y);
     }
 
-    int max(int x, int y){
-        return x>y? x:y;
-    }
-//
-//    public Node<E> rotateRight(Node<E> root){
-//        Node<E> x=root.getLeft();
-//        Node<E> y=x.getRight();
-//
-//        x.setRight(root);
-//        root.setLeft(y);
-//
-//        root.setHeight(max(root.getLeft().getHeight(), root.getRight().getHeight()) + 1);
-//        x.setHeight(max(x.getLeft().getHeight(), x.getRight().getHeight()) + 1);
-//
-//        return x;
-//    }
-//
-//    public Node<E> rotateLeft(Node<E> root){
-//        Node<E> x=root.getRight();
-//        Node<E> y=x.getLeft();
-//
-//        x.setLeft(root);
-//        root.setRight(y);
-//
-//        root.setHeight(max(root.getLeft().getHeight(), root.getRight().getHeight()));
-//        x.setHeight(max(x.getLeft().getHeight(), x.getRight().getHeight()));
-//
-//        return x;
-//    }
-
-    private int getBalanceFactor(Node<E> node){
-        if(node==null)
+    private int getBalanceFactor(Node<E> node) {
+        if (node == null)
             return 0;
-        return node.getLeft().getHeight() - node.getRight().getHeight();
+        return node.left.height - node.right.height;
     }
 
     @Override
@@ -306,11 +241,11 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
     @Override
     public boolean removeAll(Collection<?> collection) {
         boolean modified = false;
-        for(Object o : collection) {
-            if(o == null)
+        for (Object o : collection) {
+            if (o == null)
                 throw new NullPointerException("Null elements are not allowed");
 
-            if(search(o, root) != null) {
+            if (search(o, root) != null) {
                 delete(o, root);
                 modified = true;
             }
@@ -333,17 +268,15 @@ public class AVLTree<E> extends BalancedTreeSet<E> {
         return maximum(root);
     }
 
-    public static void main(String args[]){
-        AVLTree<Integer> tree =new AVLTree<>();
+    public static void main(String args[]) {
+        AVLTree<Integer> tree = new AVLTree<>();
 
         tree.add(5);
         tree.add(7);
 
         System.out.println(tree.contains(5) + " " + tree.contains(7));
 
-
         tree.displayTree(tree.root);
-
     }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -3,53 +3,23 @@
  */
 class Node<E> {
     private E data;
-    private int height;
-    private Node<E> left;
-    private Node<E> right;
+    int height;
+    Node<E> left;
+    Node<E> right;
 
-    public int getHeight() {
-        return height;
+    Node(E data) {
+        this(data, null, null);
     }
 
-    public void setData(E data) {
+
+    Node(E data, Node<E> left, Node<E> right) {
         this.data = data;
-    }
-
-    public Node<E> getRight() {
-
-        return right;
-    }
-
-    public void setRight(Node<E> right) {
-        this.right = right;
-    }
-
-    public Node<E> getLeft() {
-
-        return left;
-    }
-
-    public void setLeft(Node<E> left) {
-        this.left = left;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public Node(E data) {
-        this.data = data;
-        height = -1;
-        left = right = null;
-    }
-
-    public Node(E data, Node<E> left, Node<E> right) {
-        this(data);
         this.left = left;
         this.right = right;
     }
 
-    public E getData() {
+    E getData() {
         return data;
     }
+
 }
